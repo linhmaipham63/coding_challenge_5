f85ad242e8a1b28e061ec50aa96a1c48641df027
Linh Pham <linhmaipham@usf.edu>
2025-02-09 17:09:17 -0500

    Task 1 - Customer Profile

---
+++ cc_5.js
@@ -0,0 +1,10 @@
+// Task 1: Object Properties
+
+// Declare object customer
+let customer = {name: "John Doe", age: 35, email: "john.doe@email.com"};
+
+// Log each property 
+console.log(`Customer name: ${customer.name}`);
+console.log(`Customer age: ${customer.age}`);
+console.log(`Customer email: ${customer.email}`);
+
